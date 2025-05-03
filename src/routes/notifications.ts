4ab0ead70247ec2a598683ae7c7199eb4519ad5e
Adrian Valenzuela <avgvalenzuela@gmail.com>
2025-05-03 08:31:01 +0200

    Implement 400 bad request response using Zod to validate payload

--- src/routes/notifications.ts
+++ src/routes/notifications.ts
@@ -1,12 +1,25 @@
 import { Router } from 'express';
+import { z } from 'zod';
 
 const notificationsRouter = Router();
 
+// Define the schema for the notification payload
+const NotificationSchema = z.object({
+    "recipientId": z.string(),
+    "notificationTtype": z.string(),
+    "content": z.string(),
+});
+
+
 // POST /notifications
 notificationsRouter.post('/', (req, res) => {
-    res.status(200).json({
-        message: 'Notification received',
-    });
+    // Validate the request body with the schema
+    const valid = NotificationSchema.safeParse(req.body);
+    if (!valid.success) {
+        res.status(400).json({});
+    } else {
+        res.status(200).json({ message: 'Notification received' })
+    }
 });
 
 export default notificationsRouter;

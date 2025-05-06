0cbcec840c353625380b1d82815938ebbfdacdd5
Adrian Valenzuela <avgvalenzuela@gmail.com>
2025-05-06 20:35:43 +0200

    fix routes using absolute route with path

--- src/utils/logNotifications.ts
+++ src/utils/logNotifications.ts
@@ -1,10 +1,11 @@
 import fs from 'fs';
-
-const logFilePath = 'notificationLog.txt';
+import path from 'path';
 
 // Function to log notifications to a file
 // appendFileSync check if the file exists, if not it creates it
 export function logNotification(recipientId: string, notificationTtype: string, content: string) {
+    const logFilePath = path.resolve(__dirname, '../../notifications.log');
+
     const newLog = `Recipient: ${recipientId}, Type: ${notificationTtype}, Content: ${content}\n`;
     fs.appendFileSync(logFilePath, newLog);
 }
